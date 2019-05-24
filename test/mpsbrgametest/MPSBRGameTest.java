7ecf2d9002dcc492b541431e2fa733bb2516ee7e
Aluno <Aluno@L306-7C.lab.local>
2019-05-24 20:37:18 -0300

    Método jogar na Classe Game com TDD

--- test/mpsbrgametest/MPSBRGameTest.java
+++ test/mpsbrgametest/MPSBRGameTest.java
@@ -7,18 +7,41 @@ package mpsbrgametest;
 
 import mpsbrgame.*;
 import org.junit.*;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author Aluno
  */
 public class MPSBRGameTest {
+    private Player p1;
+    private Player p2;
+    private Game jogo;
+    
+    @Before
+    public void inicializaJogo(){
+    jogo = new Game();
+    
+    p1 = new Player("P1");
+    p2 = new Player("P2");
+    
+    jogo.addPlayer(p1);
+    jogo.addPlayer(p2);
+    
+    p1.takeCard(new Card(Suit.CLUBS, Rank.FOUR));
+    p1.takeCard(new Card(Suit.CLUBS, Rank.FIVE));
+    p1.takeCard(new Card(Suit.CLUBS, Rank.SIX));
+    
+    p2.takeCard(new Card(Suit.CLUBS, Rank.FIVE));
+    p2.takeCard(new Card(Suit.CLUBS, Rank.FOUR));
+    p2.takeCard(new Card(Suit.CLUBS, Rank.SEVEN));
+    
+    
+    }
     
     @Test
-    public void comprarCartaTest(){
-        Deck baralho = new Deck();
-        baralho.addCards();
-        Card carta = baralho.dealCard();
-        System.out.println("Carta "+carta.getValue()+" de "+carta.getSuit());
+    public void testaJogarSimples(){
+        Player ganhador = jogo.jogar(p1.getCard(1), p2.getCard(1));
+        assertEquals(p1, ganhador);
     }
 }
